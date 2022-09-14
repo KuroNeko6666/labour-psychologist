f3dc0b9214cff9df67895f4b2338ac51ffbbbbdd
“Agung <agungpriyatno666@email.com>
2022-09-14 17:14:05 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Psychotest;
+use App\Models\Jadwal;
+use App\Models\JadwalsUser;
 use App\Models\PsychotestParticipant;
 
 class HomeController extends Controller
@@ -14,7 +16,7 @@ class HomeController extends Controller
             'active' => 'dashboard',
             'title' => 'Labour | Psikotest',
             'jadwal' => Jadwal::latest()->filter(['id' => $id])->get(),
-            'participant' => PsychotestParticipant::latest()->filter(['psychologist' => $id,'status' => 'unfinished'])->get()
+            'participant' => JadwalsUser::latest()->filter(['psychologist' => $id,'status' => 'unfinished'])->get()
         ]);
     }
 }
